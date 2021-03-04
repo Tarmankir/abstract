7df0558bcbfe2d52f5c707302e51aef0343afa49
k.nosov <k.nosov@rabota.ru>
2021-03-04 12:34:06 +0300

    release good

---
+++ src/main/java/Footer.java
@@ -0,0 +1,7 @@
+public abstract class Footer implements AuthorInform {
+    public int footerPhone;
+
+    public FooterMetod() {
+        footerPhone = 5841624;
+    }
+}
---
+++ src/main/java/Page1.java
@@ -0,0 +1,18 @@
+public class Page1 extends Footer {
+    @Override
+    public void ShowAuthor() {
+        System.out.println("ФИО автора теста");
+    }
+    public int button;
+
+    public Button1() {
+        button = 1234;
+    }
+
+    public static class Id {
+        public int id;
+
+        public Id() {
+            id = 199;
+        }
+}
